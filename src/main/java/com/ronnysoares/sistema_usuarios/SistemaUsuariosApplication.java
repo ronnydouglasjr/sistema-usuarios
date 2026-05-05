9461ac9f8aad0258f16dd89212ebc8f0b1b502bc
Ronny <ronnydougla2@gmail.com>
2026-05-05 11:10:17 -0300

    commit inicial

---
+++ src/main/java/com/ronnysoares/sistema_usuarios/SistemaUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.ronnysoares.sistema_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaUsuariosApplication.class, args);
+	}
+
+}
